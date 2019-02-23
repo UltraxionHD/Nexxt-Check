347dd831fbfd356291cbcf510f5d21512988b39a
Daniel Rusche <DanielRusche@ctos.de>
2019-02-23 14:41:30 +0100

    Master Server : Startfenster angepasst

--- Master-Server/WpfApp1/MainWindow.xaml.cs
+++ Master-Server/WpfApp1/MainWindow.xaml.cs
@@ -13,16 +13,33 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace MasterServer
 {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Variablen
+
+        //Bildschirmgröße
+        private double Bildschirm_Breite = 800;
+        private double Bildschirm_Hohe = 600;
+
+        //Variablen ENDE
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //Bildschirmgröße ermitteln:
+            Bildschirm_Breite = System.Windows.SystemParameters.PrimaryScreenWidth;
+            Bildschirm_Hohe = System.Windows.SystemParameters.PrimaryScreenHeight;
+
+            //Fenstergröße auf 75% setzen:
+            this.Width = Bildschirm_Breite / 100 * 75;
+            this.Height = Bildschirm_Hohe / 100 * 75;
         }
     }
 }
